3171e278f9968df81ec49d4dd54b1b6ced0cee53
Matt Gale <matt@littleball.co.uk>
2015-04-12 17:09:59 +0100

    Got repo to a base state

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,51 @@
 var gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    jade = require('gulp-jade'),
-    babel = require('gulp-babel');
+  browserSync = require('browser-sync'),
+  jade = require('gulp-jade'),
+  babel = require('gulp-babel'),
+  sass = require('gulp-sass'),
+  reload = browserSync.reload;
 
 gulp.task('localhost', ['build'], function () {
 
-    browserSync({
-        server: {
-            baseDir: './dest'
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: './dest'
+    }
+  });
+
+  gulp.watch('src/js/**/*', ['scripts']);
+  gulp.watch('src/templates/**/*', ['templates']);
+  gulp.watch('src/scss/**/*', ['styles']);
 
 });
 
 gulp.task('scripts', function () {
-  return gulp.src('src/js/base.js')
-    .pipe(gulp.dest('dest/js'));
+  return gulp.src('src/js/app.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dest/js'))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 gulp.task('templates', function () {
     return gulp.src('src/templates/*.jade')
         .pipe(jade())
-        .pipe(gulp.dest('dest'));
+        .pipe(gulp.dest('dest'))
+        .pipe(reload({
+          stream: true
+        }));
 });
 
 gulp.task('styles', function () {
+  return gulp.src('src/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dest/css'))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 gulp.task('build', ['scripts', 'templates', 'styles']);
-
-gulp.task('watch', function () {
-
-});
 
 gulp.task('default', ['localhost']);
